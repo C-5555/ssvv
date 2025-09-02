bd707cc0846247d56d5a18270b3af2a83435ba80
Carla <carla.dgs@hotmail.com>
2025-09-02 12:42:56 -0600

    fix: actualizar permisos de archivos

---
+++ database/seeders/EmpleadoSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EmpleadoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table ('empleados')->insert([
+            [
+            'Nombre' =>'Juan',
+            'Apellido_paterno' => 'X',
+            'Apellido_materno' => 'Y',
+            'id_departamento' => '4', 
+            'Puesto'=> 'Supervisor',
+            'Fecha_ingreso' => '22-06-2024',
+            'Email'=> 'juan@gmail.com',
+            'RFC' => '5544875698AS',
+            'Foto'=> '.',
+            ]   
+
+
+
+            ]);
+    }
+}
